776510c9e73fb7988ca2dbae771643ca05e08573
Dáire Mahon <dairemahon@Daires-MacBook-Pro.local>
2026-06-05 18:49:08 +0100

    database setup and created front and back of flash card models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class FlashCardFront(models.Model):
+    front_text = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField("date published")
+
+class FlashCardBack(models.Model):
+    # defining relationship
+    question = models.ForeignKey(FlashCardFront, on_delete=models.CASCADE)
+    back_text = models.CharField(max_length=1000)
+    # votes = models.IntegerField(default=0)
