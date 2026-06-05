2d3399ea1703bbf43b8eef4b58db8561380a0901
Dáire Mahon <dairemahon@Daires-MacBook-Pro.local>
2026-06-05 19:20:29 +0100

    made the polls app modifiable by the admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import FlashCardFront
+
+admin.site.register(FlashCardFront)
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class FlashCardFront(models.Model):
     front_text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.front_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class FlashCardBack(models.Model):
     # defining relationship
     question = models.ForeignKey(FlashCardFront, on_delete=models.CASCADE)
     back_text = models.CharField(max_length=1000)
     # votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.back_text
