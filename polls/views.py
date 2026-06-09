e17ebb0665a70460202a42976a2a8c20b087a608
Dáire Mahon <dairemahon@Daires-MacBook-Pro.local>
2026-06-09 23:04:41 +0100

    added auth urls and signup view

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 
 
+def signup(request):
+        if request.method == "POST":
+                form = UserCreationForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                        return redirect("login")
+        else:
+                form = UserCreationForm()
+        return render(request, "registration/signup.html", {"form": form})
 
 
 
 def index(request):
-        return HttpResponse("You're looking at the Add page.")
+        return HttpResponse("You're looking at the index page.")
 
 def account(request):
         return HttpResponse("You're looking at the account page.")
